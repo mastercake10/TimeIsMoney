82609ef31abe103b38f38bf776d2e1a921b71702
linus <linus@xlinus-rechner>
2016-03-26 16:48:20 +0100

    Added chances

--- TimeIsMoney/src/de/Linus122/TimeIsMoney/Main.java
+++ TimeIsMoney/src/de/Linus122/TimeIsMoney/Main.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
-<<<<<<< HEAD
 import java.net.URL;
 import java.sql.Timestamp;
 import java.util.ArrayList;
